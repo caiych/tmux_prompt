6ea720e8adc557334a681d47c9214ab6bca05059
caiych <caiych@gmail.com>
2015-08-22 15:53:02 +0800

    Update a ton of docstring. As well as command line flags

--- prompt_tmux.py
+++ prompt_tmux.py
@@ -3,11 +3,24 @@
 import os
 import sys
 import urwid
+import argparse
 
 
 class TuiPrompt:
+  """A text user interface, prompt to choose a tmux session.
+  """
 
   def AvailableChoice(self, hide_attached):
+    """Get all available sessions from command 'tmux ls'
+
+    Args:
+      hide_attached: set to True to skip sessions already attached by some
+        client
+
+    Returns:
+      a list of (session name, displayed text)
+
+    """
     cmd = 'tmux ls'
     if hide_attached:
       cmd += ' | grep -v "(attached)"'
@@ -15,15 +28,37 @@ class TuiPrompt:
     res = list(os.popen(cmd))
     return map(lambda x: (x.split(':')[0], x[:-1]), res)
 
-  def item_chosen(self, button, c):
+  def ItemChosen(self, button, c):
+    """Callback function for an action item.
+
+    Will be called by urwid, when the button is hit.
+    Set the self._final_choice and end the loop.
+
+    Args:
+      button: the urwid button item, automatically passed by urwid, never used
+        in this method.
+      c: session name
+    Returns:
+      An exit exception. Will be captured by urwid main loop.
+
+    """
     self._final_choice = c
     raise urwid.ExitMainLoop()
 
   def WrapButtons(self, button_content_list):
+    """Generate the urwid widget to display
+
+    Args:
+      button_content_list: genreated by self.AvailableChoice, a list of (session
+        name, display text)
+
+    Returns:
+      a urwid widget, ready to render.
+    """
     button_list = []
     for action, button_text in button_content_list:
       button = urwid.Button(button_text)
-      urwid.connect_signal(button, 'click', self.item_chosen, action)
+      urwid.connect_signal(button, 'click', self.ItemChosen, action)
       button_list.append(urwid.AttrMap(button, None, focus_map='reversed'))
     return urwid.Overlay(
         urwid.ListBox(urwid.SimpleFocusListWalker(button_list)),
@@ -36,12 +71,23 @@ class TuiPrompt:
         min_height=9)
 
   def __init__(self, hide_attached=True, skip_choice=True):
+    """Construct the prompt object to start the TUI.
+
+    Args:
+      hide_attached: set to True to skip sessions attached by some clients.
+      skip_choice: set to True when there's only one choice to make.
+    Returns:
+      None
+    """
     available = self.AvailableChoice(hide_attached)
     if skip_choice:
       if len(available) == 0:
+        # When there's no session, the only choice is to create a new one.
         self._final_choice = ''
         return
       elif len(available) == 1:
+        # When there's only one session, the only choice is to attach to this
+        # one.
         self._final_choice = available[0][0]
         return
     urwid.MainLoop(
@@ -52,40 +98,29 @@ class TuiPrompt:
     return self._final_choice
 
 
-def OldSchoolShowAndType():
-  os.popen('clear')
-  res = list(os.popen('tmux list-sessions | grep -v "(attached)"'))
-  l = len(res)
-  if l == 0:
-    return ''
-  if l == 1:
-    return res[0].split(':')[0]
-  else:
-    print '\n'.join(res)
-    valid_sess = map(lambda x: x.split(':')[0], res)
-    while True:
-      resp = raw_input()
-      if resp == '':
-        return valid_sess[0]
-      else:
-        if resp in map(lambda x: x.split(':')[0], res):
-          return resp
-        else:
-          #print 'enter a valid session name'
-          return ''
-
-
 if __name__ == '__main__':
-  fast_start = True
+  flags_parser = argparse.ArgumentParser(
+      description=
+      'Prompt to choose a tmux session, cannot be run within a tmux window.')
+  flags_parser.add_argument(
+      '--no_fast_start',
+      action='store_true',
+      help='Switch this flag to let the program shows all sessions(including '
+      'attached), and not skip choosing anyway')
+  flags_parser.add_argument(
+      '--no_prompt_again',
+      action='store_true',
+      help='Swith this flag to let the program exit after detach sessions.')
+  flags = flags_parser.parse_args()
+
+  fast_start = not flags.no_fast_start
   while True:
     sess_name = TuiPrompt(hide_attached=fast_start,
                           skip_choice=fast_start).get()
     if sess_name == '':
       os.popen('tmux')
     else:
-      if len(sys.argv) == 1 or len(sys.argv[1]) == 0:
-        t = """tmux new-window -t %s:0 \; send-keys -t %s:0 "prodaccess && echo 'closing in 5s' && sleep 5 && exit" Enter""" % (
-            sess_name, sess_name)
-        os.popen(t)
-      os.popen('tmx %s' % (sess_name,))
+      os.popen('tmux attach -t %s' % (sess_name,))
     fast_start = False
+    if flags.no_prompt_again:
+      break
